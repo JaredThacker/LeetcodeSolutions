5b99b634c82027a9804d6d1e8551e955294c958d
Jared <jared@Zipcoders-MacBook-Pro-4.local>
2024-08-02 23:08:36 -0400

    palindrome number

--- src/main/java/Easy/PalindromeOfNumber.java
+++ src/main/java/Easy/PalindromeOfNumber.java
@@ -1,4 +1,9 @@
 package Easy;
 
 public class PalindromeOfNumber {
+    public boolean isPalindrome(int x) {
+        String reg = String.valueOf(x);
+        StringBuilder sb = new StringBuilder(reg);
+        return sb.reverse().toString().equals(reg);
+    }
 }
