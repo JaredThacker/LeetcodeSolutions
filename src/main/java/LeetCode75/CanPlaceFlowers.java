25027dab77e09f71daf271bcef5abb7eb7e74491
Jared <jared@Zipcoders-MacBook-Pro-4.local>
2024-08-06 01:48:27 -0400

    canplaceflowers solution

--- src/main/java/LeetCode75/CanPlaceFlowers.java
+++ src/main/java/LeetCode75/CanPlaceFlowers.java
@@ -4,8 +4,24 @@ public class CanPlaceFlowers {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
         int canPlant = 0;
         for (int i = 0; i < flowerbed.length; i++){
-            if (i > 0 && flowerbed[i] == 0 && flowerbed[i + 1] == 0 && flowerbed[i - 1] == 0){
-                canPlant++;
+            if (flowerbed[i] == 0){
+                boolean emptyLeftPlot = (flowerbed[i] == 0) && i > 0 && (flowerbed[i - 1] == 0);
+                boolean emptyRightPlot = (flowerbed[i] == 0) && i < flowerbed.length - 1 &&(flowerbed[i + 1] == 0);
+
+                if(emptyLeftPlot && emptyRightPlot) {
+                    flowerbed[i] = 1;
+                    canPlant++;
+                } else if (i == 0 && emptyRightPlot) {
+                    flowerbed[i] = 1;
+                    canPlant++;
+                } else if (i == flowerbed.length - 1 && emptyLeftPlot) {
+                    flowerbed[i] = 1;
+                    canPlant++;
+                } else if (flowerbed.length == 1 && flowerbed[i] == 0) {
+                    flowerbed[i] = 1;
+                    canPlant++;
+                }
+
             }
         }
         return canPlant >= n;
